d34d8c986fe7c6d0b22406ecd7b2fee437cc7e98
ANAS EL KHIAT <anas@adame-techs.com>
2024-01-27 23:12:58 +0100

    conception de la page de realisation

--- js/realisations.js
+++ js/realisations.js
@@ -4,11 +4,36 @@ let tableRealisations = document.getElementById("realisations");
 // Tableau des compétences
 let realisations = [
     {
-        nom: "ReactJS",
-        image: "../img/realisation/...",
-        lien: "",
+        nom: "H-A Conduite",
+        image: "../img/realisations/h-a-conduite.png",
+        lien: "https://anas7823.github.io/H.A-Conduite/",
+        description: "Site vitrine pour une auto-école.",
         etat: false, // true = en ligne, false = en cours de développement
         visible: true
+    },
+    {
+        nom: "GFM ASSISTANCE ADMINISTRATIVE",
+        image: "../img/realisations/gfm.webp",
+        lien: "https://anas7823.github.io/assistAd/",
+        description: "Site vitrine pour une entreprise d'aide administrative.",
+        etat: false, // true = en ligne, false = en cours de développement
+        visible: true
+    },
+    {
+        nom: "Formulaire Start-Zup",
+        image: "../img/realisations/startZup.jpg",
+        lien: "",
+        description: "Formulaire d'inscription pour le cfa Start-Zup.",
+        etat: false, // true = en ligne, false = en cours de développement
+        visible: false
+    },
+    {
+        nom: "Dashboard Start-Zup",
+        image: "../img/realisations/startZup.jpg",
+        lien: "",
+        description: "Site permettant la gestion des étudiants et leurs placement après formation.",
+        etat: false, // true = en ligne, false = en cours de développement
+        visible: false
     },
 ];
 
@@ -16,14 +41,15 @@ let realisations = [
 for (let i = 0; i < realisations.length; i++) {
     let realisation = realisations[i];
     let realisationHTML = `
-    <div class="card card-realisation" style="width:20rem; color: black; margin: 1rem; height: 20rem;">
+    <div class="card card-realisation" style="width:20rem; color: black; margin: 1rem; height: 25rem;">
             <div class="card-header" style="min-height: 4rem; ">
                 ${realisation.etat === true ? `<i class="fas fa-check-circle"  style='float: right; font-size: 30px; color: green;'></i>` : `<i class="fas fa-exclamation-triangle" style='float: right; font-size: 30px; color: #ffc107;'></i>`}
             </div>
-            <img src="${realisation.image}" class="card-img-top" alt="realisation-img" style="min-height: 160px; max-height: 10rem;">
+            <img src="${realisation.image}" class="card-img-top" alt="realisation-img" style="min-height: 160px;">
             <div class="card-body">
-                <h5 class="card-title"><b>${realisation.nom}</b></h5>
-                ${realisation.visible ? `<a href='${realisation.lien}' class="btn btn-dark">Voir le projet</a>` : ``}
+                <h5 class="card-title" style="height: 35px; ">${realisation.nom}</h5>
+                <p class="card-text" style="height: 30px; margin-bottom: 25px;">${realisation.description}</p>
+                ${realisation.visible ? `<a href='${realisation.lien}' class="btn btn-dark" target="_blank">Voir le projet</a>` : ``}
                 
             </div>
         </div>

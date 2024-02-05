5ba917c1dd37f8067ed8637878ca621a470f0af3
ANAS EL KHIAT <anas@adame-techs.com>
2024-02-05 20:48:25 +0100

    documentations

--- js/documentations.js
+++ js/documentations.js
@@ -17,3 +17,33 @@ $(document).ready(function() {
 
     console.log("La mise en place est finie. En attente d'événements...");
 });
+
+// Fonction pour appliquer des styles aux éléments correspondants à la recherche
+function highlightElements(searchTerm) {
+    // Sélectionner tous les éléments dont l'ID contient la valeur de la recherche
+    let elements = document.querySelectorAll('[id*=' + searchTerm + ']');
+  
+    if (elements.length === 0) {
+        console.log("Aucun élément trouvé.");
+        return;
+    }
+    // Appliquer les styles aux éléments trouvés
+    elements.forEach(function(element) {
+      element.classList.add('highlighted');
+      console.log("Élément trouvé : " + element.id);
+    });
+}
+
+// Écouter les changements dans l'URL
+window.addEventListener('hashchange', function() {
+    // Récupérer la valeur de l'URL après le caractère #
+    var searchTerm = window.location.hash.substring(1);
+
+    // Réinitialiser les styles
+    document.querySelectorAll('.highlighted').forEach(function(element) {
+        element.classList.remove('highlighted');
+    });
+
+    // Appliquer les styles aux éléments correspondants
+    highlightElements(searchTerm);
+});
--- readme.md
+++ readme.md
@@ -1,2 +1,4 @@
 Penser à ajouter les liens des documentations sur la card de chaque projet 
 Penser à mettre un id à chaque cazrd de doc dans documentation.html
+Changer le lien du "ici" (dernier mot de la 1er doc GLPI ) vers le nouveaux
+Liens des veilles tech à mettre
